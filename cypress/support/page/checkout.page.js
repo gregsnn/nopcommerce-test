20630d1b6795ec64be465509f3aeafe23188940a
Aron Adams <aronrapetto@hotmail.com>
2024-02-17 20:24:07 -0300

    feat: mapeamento de seletores do checkout

---
+++ cypress/support/page/checkout.page.js
@@ -0,0 +1,34 @@
+const linkNomeProduto =
+  "#shopping-cart-form > div.table-wrapper > table > tbody > tr > td.product > a";
+const inputQuantidade =
+  "#shopping-cart-form > div.table-wrapper > table > tbody > tr > td.quantity > input";
+const btnRemoverProduto =
+  "#shopping-cart-form > div.table-wrapper > table > tbody > tr > td.remove-from-cart > button";
+const preco =
+  "#shopping-cart-form > div.table-wrapper > table > tbody > tr > td.unit-price > span";
+const precoTotal =
+  "#shopping-cart-form > div.table-wrapper > table > tbody > tr > td.subtotal > span";
+
+const btnAtualizaCarrinho = "#updatecart";
+
+const inputCupom = "#discountcouponcode";
+const btnAplicaCupom = "#applydiscountcouponcode";
+const inputGiftCard = "#giftcardcouponcode";
+const btnApplyGiftCard = "#applygiftcardcouponcode";
+
+const termsCheckbox = "#termsofservice";
+
+const btnCheckout = "#checkout";
+
+const selectPais = "#BillingNewAddress_CountryId";
+const selectEstado = "#BillingNewAddress_StateProvinceId";
+const inputCidade = "#BillingNewAddress_City";
+const inputEndereco = "#BillingNewAddress_Address1";
+const inputZipCode = "#BillingNewAddress_ZipPostalCode";
+const inputTelefone = "#BillingNewAddress_PhoneNumber";
+const btnContinuarEndereco = "#billing-buttons-container > button.button-1.new-address-next-step-button"
+
+const btnContinuarMetodoEntrega = "#shipping-method-buttons-container > button"
+const btnContinuarMetodoPagamento = "#payment-method-buttons-container > button";
+const btnContinuarInfo = "#payment-info-buttons-container > button"
+const btnConfirmarCheckout = "#confirm-order-buttons-container > button";
