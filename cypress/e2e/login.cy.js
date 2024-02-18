111415667bf5d98f671abdee21a8e5595bd3d24a
Gregori Nunes <gregorisnunes@outlook.com>
2024-02-17 21:29:06 -0300

    test: adicionando cenario feliz de login e ajustando setup

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,1 +1,17 @@
 /// <reference types="cypress" />
+
+describe("Login", () => {
+  beforeEach(() => {
+    cy.visit("/");
+
+    cy.setupLogin();
+  });
+
+  it.only("com dados validos", () => {
+    cy.navegarParaLogin();
+
+    cy.readFile("cypress/fixtures/login.json").then((data) => {
+      cy.logar(data.usuario_valido);
+    });
+  });
+});
--- cypress/utils/base.usuario.js
+++ cypress/utils/base.usuario.js
@@ -1,23 +1,36 @@
 import faker from "@faker-js/faker";
 
-const checkboxArray = ["check", "uncheck"];
-const genderArray = ["male", "female"];
-const date = faker.date.between(1914, 2024);
-const password = faker.internet.password();
+const OPCOES_CHECKBOX = ["check", "uncheck"];
+const OPCOES_GENERO = ["male", "female"];
+const INITIAL_DATE = new Date(1914, 1, 1);
+const FINAL_DATE = new Date(2024, 12, 31);
 
-export const usuario = () => {
+export const registro = () => {
+  const date = faker.date.between(INITIAL_DATE, FINAL_DATE);
+  const password = faker.internet.password();
+
   return {
-    genero: random(genderArray),
+    genero: random(OPCOES_GENERO),
     nome: faker.name.firstName(),
     sobrenome: faker.name.lastName(),
-    diaNascimento: date.getDay(),
-    mesNascimento: date.getMonth(),
+    diaNascimento: date.getUTCDate(),
+    mesNascimento: date.getUTCMonth(),
     anoNascimento: date.getFullYear().toString(),
     email: faker.internet.email(),
     nomeEmpresa: faker.company.companyName(),
-    newsletter: random(checkboxArray),
+    newsletter: random(OPCOES_CHECKBOX),
     senha: password,
     confirmarSenha: password,
+  };
+};
+
+export const login = (usuarioRegistrado) => {
+  const { email, senha } = usuarioRegistrado;
+
+  return {
+    email,
+    senha,
+    lembrarUsuario: random(OPCOES_CHECKBOX),
   };
 };
 
