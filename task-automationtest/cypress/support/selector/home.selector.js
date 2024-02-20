00e8eb475ea79c3adf58a5976b7793a571edc7d4
Aron Adams <aronrapetto@hotmail.com>
2024-02-20 00:15:14 -0300

    feat: seletores da pagina home, login e cadastro

---
+++ task-automationtest/cypress/support/selector/home.selector.js
@@ -0,0 +1,7 @@
+export default {
+  btnLinkParaLogin: "#header > div.nav > div > div > nav > div.header_user_info > a",
+  btnDeslogar: "#header > div.nav > div > div > nav > div:nth-child(2) > a",
+  btnLinkParaContactUs: "#contact-link",
+  btnPaginaInicial: "#center_column > ul > li > a > span",
+  btnLinkMinhaConta: "#header > div.nav > div > div > nav > div:nth-child(1) > a > span",
+}
---
+++ task-automationtest/cypress/support/selector/login.selector.js
@@ -0,0 +1,8 @@
+export default {
+  campoEmail: "#email",
+  campoSenha: "#passwd",
+  btnLogin: "#SubmitLogin > span",
+  btnCriarConta: "#SubmitCreate > span",
+  campoEmailCriarConta: "#email_create",
+  errorMessageSelector: "#center_column > div.alert.alert-danger > ol > li",
+}
